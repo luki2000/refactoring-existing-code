3e7dce0c0b002b39eb958706120788411c8ecb25
lukindo <lukindo.mbuli@gmail.com>
2020-03-24 16:40:10 +0100

    extracted credits logic and format logic

--- test.spec.js
+++ test.spec.js
@@ -1,29 +1,43 @@
+// WE ARE ON PAGE 20
+
+
 function statment(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat("en-US",{ 
-                                      style: "currency", currency: "USD",
-                                      minimumFractionDigits: 2}).format;
+  
   for(let perf of invoice.performances) {
-    let thisAmount = amountFor(perf);
 
-    //add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+    volumeCredits += volumeCreditsFor(perf);
 
     // print line for this order
-    result += ` ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
-    totalAmount += thisAmount;
+    result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
+    totalAmount += amountFor(perf);
   }
   result += `Amount owed is ${format(totalAmount/100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 }
 
-function playFor(aPerformance){
+
+
+function playFor(aPerformance) {
   return plays[aPerformance.playID];
+}
+
+function volumeCreditsFor(aPerformance) {
+  let result = 0;
+  //add volume credits
+  result += Math.max(perf.audience - 30, 0);
+  // add extra credit for every ten comedy attendees
+  if("comedy" === playFor(perf).type) result += Math.floor(perf.audience / 5);
+  return result;
+}
+
+function format(aNumber) {
+  return new Intl.NumberFormat("en-US",{ 
+    style: "currency", currency: "USD",
+    minimumFractionDigits: 2}).format(aNumber);
 }
 
 
